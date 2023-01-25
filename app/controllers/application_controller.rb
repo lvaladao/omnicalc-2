3d3d12861eb862b7369d3f9681dfcbcb13c56c14
lvmadoreira <lvaladao@chicagobooth.edu>
2023-01-25 21:18:39 +0000

    Finished calculations (add, subtract, multiply, divide)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,49 @@
 class ApplicationController < ActionController::Base
+  def add_form
+    render({ :template => "templates/add_form.html.erb"})
+  end
+  
+  def add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
 
+    render({ :template => "templates/add.html.erb"})
+  end
+
+  def subtract_form
+    render({ :template => "templates/subtract_form.html.erb"})
+  end
+
+  def subtract
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @second_num - @first_num
+
+    render({ :template => "templates/subtract.html.erb"})
+  end
+
+  def multiply_form
+    render({ :template => "templates/multiply_form.html.erb"})
+  end
+
+  def multiply
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num * @second_num
+
+    render({ :template => "templates/multiply.html.erb"})
+  end
+
+  def divide_form
+    render({ :template => "templates/divide_form.html.erb"})
+  end
+
+  def divide
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
+
+    render({ :template => "templates/divide.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/add", { :controller => "application", :action => "add_form" })
+  get("/wizard_add", { :controller => "application", :action => "add" })
+  get("/subtract", { :controller => "application", :action => "subtract_form" })
+  get("/wizard_subtract", { :controller => "application", :action => "subtract" })
+  get("/multiply", { :controller => "application", :action => "multiply_form" })
+  get("/wizard_multiply", { :controller => "application", :action => "multiply" })
+  get("/divide", { :controller => "application", :action => "divide_form" })
+  get("/wizard_divide", { :controller => "application", :action => "divide" })
 end
