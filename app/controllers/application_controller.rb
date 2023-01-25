aa29546c2847d04d4466d3ba518db6638b9621a9
lvmadoreira <lvaladao@chicagobooth.edu>
2023-01-25 21:43:50 +0000

    All tests have passed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,4 +46,62 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "templates/divide.html.erb"})
   end
+
+  def coordinates_form
+    render({ :template => "templates/coordinates_form.html.erb"})
+  end
+
+  def coordinates
+    @address = params.fetch("user_street_address")
+    gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "&key=" + ENV.fetch("GMAPS_KEY")
+    gmaps_parsed_data = JSON.parse(URI.open(gmaps_api_endpoint).read)
+    gmaps_results_array = gmaps_parsed_data.fetch("results")
+    loc = gmaps_results_array[0].fetch("geometry").fetch("location")
+    @latitude = loc.fetch("lat")
+    @longitude = loc.fetch("lng")
+
+    render({ :template => "templates/coordinates.html.erb"})
+  end
+
+  def weather_form
+    render({ :template => "templates/weather_form.html.erb"})
+  end
+
+  def weather
+    @latitude = params.fetch("user_latitude")
+    @longitude = params.fetch("user_longitude")
+    darksky_api_endpoint = "https://api.darksky.net/forecast/" + ENV.fetch("DARK_SKY_KEY") + "/" + @latitude + "," + @longitude
+    darksky_parsed_data = JSON.parse(URI.open(darksky_api_endpoint).read)
+    @temperature = darksky_parsed_data.fetch("currently").fetch("temperature")
+    @summary = darksky_parsed_data.fetch("currently").fetch("summary")
+    @outlook1 = darksky_parsed_data.fetch("minutely").fetch("summary")
+    @outlook2 = darksky_parsed_data.fetch("hourly").fetch("summary")
+    @outlook3 = darksky_parsed_data.fetch("daily").fetch("summary")
+
+    render({ :template => "templates/weather.html.erb"})
+  end
+
+  def coords_weather_form
+    render({ :template => "templates/coords_weather_form.html.erb"})
+  end
+
+  def coords_weather
+    @address = params.fetch("user_street_address")
+    gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "&key=" + ENV.fetch("GMAPS_KEY")
+    gmaps_parsed_data = JSON.parse(URI.open(gmaps_api_endpoint).read)
+    gmaps_results_array = gmaps_parsed_data.fetch("results")
+    loc = gmaps_results_array[0].fetch("geometry").fetch("location")
+    @latitude = loc.fetch("lat")
+    @longitude = loc.fetch("lng")
+    
+    darksky_api_endpoint = "https://api.darksky.net/forecast/" + ENV.fetch("DARK_SKY_KEY") + "/" + @latitude.to_s + "," + @longitude.to_s
+    darksky_parsed_data = JSON.parse(URI.open(darksky_api_endpoint).read)
+    @temperature = darksky_parsed_data.fetch("currently").fetch("temperature")
+    @summary = darksky_parsed_data.fetch("currently").fetch("summary")
+    @outlook1 = darksky_parsed_data.fetch("minutely").fetch("summary")
+    @outlook2 = darksky_parsed_data.fetch("hourly").fetch("summary")
+    @outlook3 = darksky_parsed_data.fetch("daily").fetch("summary")
+
+    render({ :template => "templates/coords_weather.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   get("/wizard_multiply", { :controller => "application", :action => "multiply" })
   get("/divide", { :controller => "application", :action => "divide_form" })
   get("/wizard_divide", { :controller => "application", :action => "divide" })
+
+  get("/street_to_coords/new", { :controller => "application", :action => "coordinates_form" })
+  get("/street_to_coords/results", { :controller => "application", :action => "coordinates" })
+  get("/coords_to_weather/new", { :controller => "application", :action => "weather_form" })
+  get("/coords_to_weather/results", { :controller => "application", :action => "weather" })
+  get("/street_to_weather/new", { :controller => "application", :action => "coords_weather_form" })
+  get("/street_to_weather/results", { :controller => "application", :action => "coords_weather" })
 end
